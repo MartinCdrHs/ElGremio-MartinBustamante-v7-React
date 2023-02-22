05bf17309e562d103e44d8b406e9f517d516dd7c
Martin Bustamante <martin.m.bustamante@gmail.com>
2023-02-22 14:17:16 -0300

    desarrollo de nav bar completo

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,34 +1,27 @@
+import  "./NavBar.scss"
+
+
 const NavBar = () => {
     
     return (
-      <div>
         <header>
-            <div>
-                <a href="#"> <img className="nav-img" src="./logo80x80.png" alt="logo"/> </a>
-                <nav className="">
-                    <ul className="">
-                        <li className="">
-                            <a className="" href="index.html">El Gremio</a>
-                        </li>
-                        <li className="">
-                            <a className="" href="paginas/carta.html">La Taberna</a>
-                        </li>
-                        <li className="">
-                            <a className="" href="paginas/juegos.html">El Tablón</a>
-                        </li>
-                        <li className="">
-                            <a className="" href="paginas/reservas.html">Reservas</a>
-                        </li>
-                        <li className="">
-                            <a className="" href="paginas/comollego.html">Cómo llegar</a>
-                        </li>
-                    </ul>
+            <div className="nav-container">
+
+                <a href=""> <img className="nav-img" src="./logo80x80.png" alt="logo"/> </a>
+               
+                <nav className="nav-bar">
+                    <a className="nav-link" href="">El Gremio</a>
+
+                    <a className="nav-link" href="">La Taberna</a>
+
+                    <a className="nav-link" href="">El Tablón</a>
+
+                    <a className="nav-link" href="">Reservas</a>
+
+                    <a className="nav-link" href="">Cómo llegar</a>
                 </nav>
             </div>
         </header>
-        
-        
-      </div>
     );
 }
   
