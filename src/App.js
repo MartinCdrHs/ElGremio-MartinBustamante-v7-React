19298fc7647d4559cb89235e8b47821abb7ffeb1
Martin Bustamante <martin.m.bustamante@gmail.com>
2023-02-22 16:30:01 -0300

    se agrega ItemListContainer con saludos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
 import NavBar from "./componentes/NavBar/NavBar";
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
         <NavBar/>
+        <ItemListContainer/>
     </div>
   );
 }
---
+++ src/componentes/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,20 @@
+import  "./ItemListContainer.scss"
+
+
+const ItemListContainer = () => {
+    
+    return (
+        <main className="padding-top">
+               
+            <h1 className="rojo-gremio">Pasa, pasa Aventurero! Éste es El Tablón!</h1>
+
+            <h2>Aquí podrás ver algunas de las aventuras que 
+                <span className="rojo-gremio h2-grande"> El Gremio</span> tiene para ti.
+                Recuerda que los más rápidos se llevan las mejores!
+            </h2>
+
+        </main>
+    );
+}
+  
+export default ItemListContainer;
